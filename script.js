8c9b2d921a7408ed0359247d18eee1295fc93cf3
george-gca <31376482+george-gca@users.noreply.github.com>
2024-02-05 20:33:21 +0000

    Deploying to gh-pages from @ george-gca/QR_code_generator@b165f24c9a719789ff83cfbcb245ac071bda5402 🚀

--- script.js
+++ script.js
@@ -1,53 +1,51 @@
 const wrapper = document.querySelector(".wrapper"),
-qrInput = wrapper.querySelector("#qrData"),
-qrBgColor = wrapper.querySelector("#qrBgColor"),
-qrColor = wrapper.querySelector("#qrColor"),
-generateBtn = wrapper.querySelector(".form button"),
-qrImg = wrapper.querySelector(".qr-code img");
-let preValue;
-let preBgColor;
-let preColor;
+  qrInput = wrapper.querySelector("#qrData"),
+  qrBgColor = wrapper.querySelector("#qrBgColor"),
+  qrColor = wrapper.querySelector("#qrColor"),
+  generateBtn = wrapper.querySelector(".form button"),
+  qrImg = wrapper.querySelector(".qr-code img");
+let preValue, preBgColor, preColor;
 
-IMask(
-  qrBgColor,
-  {
-    mask: 'num-num-num',
-    blocks: {
-      num: {
-        // nested masks are available!
-        mask: Number,
-        min: 0,
-        max: 255
-      }
-    }
-  }
-)
+IMask(qrBgColor, {
+  mask: "num-num-num",
+  blocks: {
+    num: {
+      // nested masks are available!
+      mask: Number,
+      min: 0,
+      max: 255,
+    },
+  },
+});
 
-IMask(
-  qrColor,
-  {
-    mask: 'num-num-num',
-    blocks: {
-      num: {
-        // nested masks are available!
-        mask: Number,
-        min: 0,
-        max: 255
-      }
-    }
-  }
-)
+IMask(qrColor, {
+  mask: "num-num-num",
+  blocks: {
+    num: {
+      // nested masks are available!
+      mask: Number,
+      min: 0,
+      max: 255,
+    },
+  },
+});
 
 generateBtn.addEventListener("click", () => {
   let qrValue = qrInput.value.trim();
   let bgColor = qrBgColor.value || "255-255-255";
   let color = qrColor.value || "0-0-0";
-  if(!qrValue || (preValue === qrValue && preBgColor === bgColor && preColor === color)) return;
+  if (
+    !qrValue ||
+    (preValue === qrValue && preBgColor === bgColor && preColor === color)
+  )
+    return;
+
   preValue = qrValue;
   preBgColor = bgColor;
   preColor = color;
   generateBtn.innerText = "Generating QR Code...";
   qrImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=200x200&format=svg&color=${color}&bgcolor=${bgColor}&data=${qrValue}`;
+
   qrImg.addEventListener("load", () => {
     wrapper.classList.add("active");
     generateBtn.innerText = "Generate QR Code";
@@ -55,7 +53,7 @@ generateBtn.addEventListener("click", () => {
 });
 
 qrInput.addEventListener("keyup", () => {
-  if(!qrInput.value.trim()) {
+  if (!qrInput.value.trim()) {
     wrapper.classList.remove("active");
     preValue = "";
   }
